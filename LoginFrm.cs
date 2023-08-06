eb4bfbe6e8502bd369b52a51b1838102708c1244
Ferney Talero <48497565+DeveloperFerneyTH@users.noreply.github.com>
2023-08-06 16:24:42 -0500

    Load source

---
+++ LoginFrm.cs
@@ -0,0 +1,46 @@
+﻿using MedicalGroup7.Core.Domain;
+using MedicalGroup7.Core.Providers;
+using System;
+using System.Windows.Forms;
+
+namespace MedicalGroup7
+{
+    public partial class LoginFrm : Form
+    {
+        UserProvider provider;
+        public LoginFrm()
+        {
+            InitializeComponent();
+            provider = new UserProvider();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtEmail.Text.Length == 0 || txtPassword.Text.Length == 0)
+                {
+                    lblMsg.Text = "Oye por fa llena los campos";
+                }
+                else
+                {
+                    UserMedical userMedical = provider.Login(txtEmail.Text, txtPassword.Text);
+                    if (userMedical == null)
+                    {
+                        lblMsg.Text = "Datos erroneos, quizas debes se te fue un dedo mal";
+                    }
+                    else
+                    {                        
+                        UserFrm userFrm = new UserFrm();
+                        userFrm.UserMedical = userMedical;
+                        userFrm.Show();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Houston tenemos un problema... error: {ex.Message}", "Oooops", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
---
+++ UserFrm.cs
@@ -0,0 +1,73 @@
+﻿using MedicalGroup7.Core.Domain;
+using MedicalGroup7.Core.Providers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MedicalGroup7
+{
+    public partial class UserFrm : Form
+    {
+        public UserMedical UserMedical { get; set; }
+        MedicalAppointmentProvider appointmentProvider;
+
+        public UserFrm()
+        {
+            InitializeComponent();
+            appointmentProvider = new MedicalAppointmentProvider();
+        }
+
+        private void UserFrm_Load(object sender, EventArgs e)
+        {
+            lblName.Text = UserMedical.FirtName;
+            lblEmail.Text = UserMedical.Email;
+            lblFirstName.Text = UserMedical.FirtName;
+            lblLastName.Text = UserMedical.LastName;
+            LoadMedicals();
+        }
+
+        private void LoadMedicals()
+        {
+            try
+            {
+                LoadData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Houston tenemos un problema... error: {ex.Message}", "Oooops", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            NewAppointmentFrm newAppointment = new NewAppointmentFrm();
+            newAppointment.UserMedical = UserMedical;
+            newAppointment.Show();
+        }
+
+        private void LoadData()
+        {
+            var medicals = appointmentProvider.GetMedicalAppointmentByUserID(UserMedical.UserID);
+            dgAppointment.DataSource = medicals;
+            dgAppointment.Refresh();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                LoadData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Houston tenemos un problema... error: {ex.Message}", "Oooops", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
